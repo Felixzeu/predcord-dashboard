5c41e6c586f048f30d24253616457a11750c1572
Felixzeu <andrea.vaudo@icloud.com>
2026-09-18 03:32:25 +0200

    Update db.js

--- db.js
+++ db.js
@@ -78,6 +78,7 @@ const GuildConfigSchema = new mongoose.Schema({
     guildId: { type: String, required: true, unique: true, index: true },
     joinLeaveLogChannelId: { type: String, default: null },
     modLogChannelId: { type: String, default: null },
+    transcriptsChannelId: { type: String, default: null },
     staffRoleId: { type: String, default: null },
     modRoleId: { type: String, default: null },
     adminRoleId: { type: String, default: null },
