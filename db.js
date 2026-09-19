ccc0d3ed09e0c06263aab46db16c7fe1dc0ee274
Felixzeu <andrea.vaudo@icloud.com>
2026-09-19 14:48:33 +0200

    Update db.js

--- db.js
+++ db.js
@@ -85,7 +85,7 @@ const GuildConfigSchema = new mongoose.Schema({
     supportCategoryId: { type: String, default: null },
     reportCategoryId: { type: String, default: null },
     staffApplicationChannelId: { type: String, default: null },
-    staffAppPredcordGuildId: { type: String, default: null },
+    banAppealChannelId: { type: String, default: null },
     dashboardPermissions: {
         createRoles: { type: [String], default: [] },
         editRoles: { type: [String], default: [] },
